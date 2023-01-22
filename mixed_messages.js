c7c5c3825bb0275ba05eadbc4647cbd8bc14fec2
Jheimbs <justinnheimbuch@gmail.com>
2023-01-22 11:29:30 -0500

    modified code

---
+++ mixed_messages.js
@@ -0,0 +1,2 @@
+let quote = ['Do or do not, there is no try.', 'Never tell me the odds!', 'Your focus determines your reality.', 'The greatest teacher failure is', 'Feel, do not think. Trust your instincts.']
+let quoteImages = ['./images/yodal.png', './image/han.webp', './image/qui1.webp', './image/yoda.jpg', './image/qui.png']
