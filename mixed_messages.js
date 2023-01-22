4839d8c138a339913ac6cc198217935033838b27
Jheimbs <justinnheimbuch@gmail.com>
2023-01-22 13:49:50 -0500

    finish js

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,2 +1,18 @@
 let quote = ['Do or do not, there is no try.', 'Never tell me the odds!', 'Your focus determines your reality.', 'The greatest teacher failure is', 'Feel, do not think. Trust your instincts.']
 let quoteImages = ['./images/yodal.png', './image/han.webp', './image/qui1.webp', './image/yoda.jpg', './image/qui.png']
+
+//Set up events to be changed
+const button = document.getElementById('button');
+
+const getQuote = document.getElementById('get-quote');
+const getMeal = () => {
+    let choice = Math.floor(Math.random()*quote.length)
+    let quoteChoice = quote[choice]
+    quoteTarget.innerHTML = quoteChoice;
+    quoteImages.src = quoteImages[choice];
+}
+ 
+
+//Makes button active
+
+button.addEventListener('click', chooseQuote)
